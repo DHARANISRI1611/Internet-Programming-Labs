aabe82e6ccfa4425648e0439b30f2cd3dd92d4b1
DHARANISRI1611 <121503033+DHARANISRI1611@users.noreply.github.com>
2024-04-23 13:25:12 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,94 +1,80 @@
 <?php
-
-class ShoppingCart
-{
-    private $items = [];
-    private $quantities = [];
-    private $totalPrice = 0;
-
-    public function addItem($item, $quantity, $price)
-    {
-        if (array_key_exists($item, $this->items)) {
-            
-            $this->quantities[$item] += $quantity;
-        } else {
-     
-            $this->items[$item] = $price;
-            $this->quantities[$item] = $quantity;
-        }
-
-        $this->totalPrice += $quantity * $price;
-    }
-
-    public function removeItem($item)
-    {
-        if (array_key_exists($item, $this->items)) {
-            $this->totalPrice -= $this->quantities[$item] * $this->items[$item];
-            unset($this->items[$item]);
-            unset($this->quantities[$item]);
-        }
-    }
-
-    public function updateQuantity($item, $quantity)
-    {
-        if (array_key_exists($item, $this->items)) {
-            $this->totalPrice -= $this->quantities[$item] * $this->items[$item];
-            $this->quantities[$item] = $quantity;
-            $this->totalPrice += $quantity * $this->items[$item];
-        }
-    }
-
-    public function getTotalPrice()
-    {
-        return $this->totalPrice;
-    }
-
-    public function getItems()
-    {
-        return $this->items;
-    }
-
-    public function getQuantities()
-    {
-        return $this->quantities;
-    }
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "job_applications";
+$port = 3306;
+$nameErr = $emailErr = $cvErr = "";
+$conn = new mysqli($servername, $username, $password, $database, $port);
+if ($conn->connect_error) {
+    echo("Connection failed: " . $conn->connect_error);
 }
-
-$cart = new ShoppingCart();
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $item = $_POST["item"];
-    $quantity = $_POST["quantity"];
-    $price = $_POST["price"];
-
-    $cart->addItem($item, $quantity, $price);
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $cv_path = $_FILES["cv"]["name"];
+    if (empty($name)) {
+        $nameErr = "Name is required";
+    }
+    if (empty($email)) {
+        $emailErr = "Email is required";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Invalid email format";
+    }
+    if (empty($cv_path)) {
+        $cvErr = "CV is required";
+    }
+    if (empty($nameErr) && empty($emailErr) && empty($cvErr)) {
+        $target_dir = "cv_uploads/";
+        $target_file = $target_dir . basename($_FILES["cv"]["name"]);
+        if (move_uploaded_file($_FILES["cv"]["tmp_name"], $target_file)) {
+            $sql = "INSERT INTO applications (name, email, cv_path) VALUES (?, ?, ?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("sss", $name, $email, $target_file);
+            if ($stmt->execute()) {
+                echo "Application submitted successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+            $stmt->close();
+        } else {
+            echo "Error uploading file";
+        }
+    }
 }
-
+$conn->close();
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Shopping Cart</title>
+    <title>Job Application Form</title>
+    <style>
+        form {
+            border: 1px solid white;
+            width: 400px;
+            height: 400px;
+            margin-top: 20px;
+        }
+
+        .error {
+            color: red;
+        }
+    </style>
 </head>
 <body>
-    <h2>Add Item to Cart</h2>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-        <label for="item">Item:</label><br>
-        <input type="text" id="item" name="item"><br><br>
-        <label for="quantity">Quantity:</label><br>
-        <input type="text" id="quantity" name="quantity"><br><br>
-        <label for="price">Price:</label><br>
-        <input type="text" id="price" name="price"><br><br>
-        <input type="submit" value="Add to Cart">
-    </form>
-
-    <h2>Shopping Cart</h2>
-    <p>Total Price: $<?php echo $cart->getTotalPrice(); ?></p>
-    <ul>
-        <?php foreach ($cart->getItems() as $item => $price): ?>
-            <li><?php echo "$item - Quantity: " . $cart->getQuantities()[$item] . " - Price: $$price"; ?></li>
-        <?php endforeach; ?>
-    </ul>
+    <center>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name">
+            <span class="error"><?php echo $nameErr; ?></span><br><br>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email">
+            <span class="error"><?php echo $emailErr; ?></span><br><br>
+            <label for="cv">CV:</label>
+            <input type="file" id="cv" name="cv">
+            <span class="error"><?php echo $cvErr; ?></span><br><br>
+            <input type="submit" value="Submit">
+        </form>
+    </center>
 </body>
 </html>
